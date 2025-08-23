ea2543d12609fd410dbc479cce42235490001c19
ana-e-uk <uribe055@umn.edu>
2025-08-22 19:32:36 -0500

    delete unneeded format code

--- frontend/src/components/CardinalDirections.jsx
+++ frontend/src/components/CardinalDirections.jsx
@@ -5,66 +5,46 @@ import '../styles/cardinaldirections.css';
 const CardinalDirections = ({ formData, handleChange }) => {
     return (
         <>
-            <div className="cardinal_container">
-                <div className="north_container">
-                    {/* <p className="cardinal_title">North</p> */}
-                    <TextField
-                        id="outlined-number"
-                        label="North"
-                        type="number"
-                        // sx={{ width:""}}
-                        className="cardinal_input"
-                        value={formData.north}
-                        name="north"
-                        onChange={handleChange}
-                        max="90"
-                        min="-90"
-                    />
-                </div>
-                <div className="ew_container">
-                    <div className="ew_sub_container">
-                        {/* <p className="cardinal_title">West</p> */}
-                        <TextField
-                            id="outlined-number"
-                            label="West"
-                            type="number"
-                            // sx={{ width:""}}
-                            max="180"
-                            min="-180"
-                            className="ew_input"
-                            name="west"
-                            value={formData.west}
-                            onChange={handleChange} />
-                    </div>
-                    <div className="ew_sub_container">
-                        {/* <p className="cardinal_title">East</p> */}
-                        <TextField
-                            id="outlined-number"
-                            label="East"
-                            type="number"
-                            // sx={{ width:""}}
-                            max="180"
-                            min="-180"
-                            name="east"
-                            className="ew_input"
-                            value={formData.east}
-                            onChange={handleChange} />
-                    </div>
-                </div>
-                <div className="north_container">
-                    {/* <p className="cardinal_title">South</p> */}
-                    <TextField
-                        id="outlined-number"
-                        label="South"
-                        max="90"
-                        min="-90"
-                        type="number"
-                        name="south"
-                        className="cardinal_input"
-                        value={formData.south}
-                        onChange={handleChange} />
-                </div>
-            </div>
+        <TextField
+            id="outlined-number"
+            label="North"
+            type="number"
+            value={formData.north}
+            name="north"
+            onChange={handleChange}
+            max="90"
+            min="-90"
+        />
+        <TextField
+            id="outlined-number"
+            label="West"
+            type="number"
+            max="180"
+            min="-180"
+            name="west"
+            value={formData.west}
+            onChange={handleChange} 
+        />
+        <TextField
+            id="outlined-number"
+            label="East"
+            type="number"
+            max="180"
+            min="-180"
+            name="east"
+            value={formData.east}
+            onChange={handleChange} 
+        />
+        <TextField
+            id="outlined-number"
+            label="South"
+            max="90"
+            min="-90"
+            type="number"
+            name="south"
+            value={formData.south}
+            onChange={handleChange} 
+        />
         </>
     )
 }
