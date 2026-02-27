48955390cf78f9cc871f71e60e3a73ef9d386e45
ana-e-uk <uribe055@umn.edu>
2026-02-27 15:23:36 -0600

    reorder execute func to account for no download files

--- backend/api/iharp_query_processor/src/query_executor_get_raster.py
+++ backend/api/iharp_query_processor/src/query_executor_get_raster.py
@@ -31,11 +31,15 @@ class GetRasterExecutor(QueryExecutor):
         # print(f"[GetRasterExecutor.get_log] log_info: {self.log_info}")
         return self.log_info
     
-    def _gen_config_dict(self):
+    def _data_files_for_query(self):
 
         df_overlap, leftover = query_get_overlap_and_leftover(self.dr)
 
-        local_files = df_overlap["file_path"].tolist()
+        if not df_overlap["file_path"]:     # TODO: check if need this
+            local_files = []
+        else:
+            local_files = df_overlap["file_path"].tolist()
+            local_files = sorted(local_files)
 
         # TODO: allow multiple leftovers
         if leftover is not None:
@@ -68,7 +72,7 @@ class GetRasterExecutor(QueryExecutor):
                     if leftover_start_month == leftover_end_month:
                         days = [str(i).zfill(2) for i in range(leftover_start_day, leftover_end_day + 1)]
 
-            config = {
+            request_params = {
                 "dataset": "carra",     # TODO: add dataset variable to dr
                 "variable": self.dr.variable,
                 "years": years,
@@ -79,8 +83,10 @@ class GetRasterExecutor(QueryExecutor):
                 "min_lon": leftover_min_lon,
                 "max_lon": leftover_max_lon,
             }
+        else:
+            request_params = {}
 
-        return local_files, config
+        return local_files, request_params
 
     def _check_metadata(self):
         """
@@ -143,76 +149,77 @@ class GetRasterExecutor(QueryExecutor):
         return f"download_{dt}.nc"
 
     def execute(self):
-        # 1. check metadata for local data and get needed data
-        local_files, config_dict = self._gen_config_dict()
 
-        # 2. call apis
-
-        # TODO: make toml method an option to pass dict to driver.py
-        # write_toml_config("request.toml", config_dict)
-        # driver = RequestRemoteData.from_toml("request.toml")
-
-        driver = RequestRemoteData.from_dict(config_dict)
-        result = driver.execute()
-        
-        if result.success:
-            downloaded_files = result.files
-        else:
-            raise RuntimeError(result.error)
-
-        # 3. execute query
+        # 1. check local data
         ds_list = []
+        local_files, params = self._data_files_for_query()
 
-        # 3.1 read downloaded files
-        for file in downloaded_files:
-            ds = xr.open_dataset(file, engine="netcdf4")
-            # drop unused variables
-            # if "number" in ds.coords:
-            #     ds = ds.drop_vars("number")
-            # if "expver" in ds.coords:
-            #     ds = ds.drop_vars("expver")
-            ds = ds.sel(
-                valid_time=slice(self.dr.start_datetime, self.dr.end_datetime),
-                latitude=slice(self.dr.max_lat, self.dr.min_lat),
-                longitude=slice(self.dr.min_lon, self.dr.max_lon),
-            )
-            # temporal resample
-            if self.dr.temporal_resolution != "hour":
-                resampled = ds.resample(valid_time=time_resolution_to_freq(self.dr.temporal_resolution))
-                if self.dr.aggregation == "mean":
-                    ds = resampled.mean()
-                elif self.dr.aggregation == "max":
-                    ds = resampled.max()
-                elif self.dr.aggregation == "min":
-                    ds = resampled.min()
-                else:
-                    raise ValueError("Invalid temporal_aggregation")
-            # spatial resample
-            if self.dr.spatial_resolution > 0.25:
-                c_f = int(self.dr.spatial_resolution / 0.25)
-                coarsened = ds.coarsen(latitude=c_f, longitude=c_f, boundary="trim")
-                if self.dr.aggregation == "mean":
-                    ds = coarsened.mean()
-                elif self.dr.aggregation == "max":
-                    ds = coarsened.max()
-                elif self.dr.aggregation == "min":
-                    ds = coarsened.min()
-                else:
-                    raise ValueError("Invalid spatial_aggregation")
-            ds_list.append(ds)
+        # 2. join local data files
+        if local_files:
+            for file in local_files:
+                ds = xr.open_dataset(file, engine="netcdf4").sel(
+                    valid_time=slice(self.dr.start_datetime, self.dr.end_datetime),
+                    latitude=slice(self.dr.max_lat, self.dr.min_lat),
+                    longitude=slice(self.dr.min_lon, self.dr.max_lon),
+                )
+                ds_list.append(ds)
 
-        # 3.2 read local files
+        # 3. if local data cannot answer query, call APIs to download needed data
+        if params:
 
-        # ds_list = []
-        for file in local_files:
-            ds = xr.open_dataset(file, engine="netcdf4").sel(
-                valid_time=slice(self.dr.start_datetime, self.dr.end_datetime),
-                latitude=slice(self.dr.max_lat, self.dr.min_lat),
-                longitude=slice(self.dr.min_lon, self.dr.max_lon),
-            )
-            ds_list.append(ds)
+            # TODO: make toml method an option to pass dict to driver.py
+            # write_toml_config("request.toml", config_dict)
+            # driver = RequestRemoteData.from_toml("request.toml")
 
-        # 3.3 assemble result
+            driver = RequestRemoteData.from_dict(params)
+            result = driver.execute()
+            
+            if result.success:
+
+                downloaded_files = result.files
+
+        # 4. join downloaded files
+                for file in downloaded_files:
+                    ds = xr.open_dataset(file, engine="netcdf4")
+                    # drop unused variables
+                    # if "number" in ds.coords:
+                    #     ds = ds.drop_vars("number")
+                    # if "expver" in ds.coords:
+                    #     ds = ds.drop_vars("expver")
+                    ds = ds.sel(
+                        valid_time=slice(self.dr.start_datetime, self.dr.end_datetime),
+                        latitude=slice(self.dr.max_lat, self.dr.min_lat),
+                        longitude=slice(self.dr.min_lon, self.dr.max_lon),
+                    )
+                    # temporal resample
+                    if self.dr.temporal_resolution != "hour":
+                        resampled = ds.resample(valid_time=time_resolution_to_freq(self.dr.temporal_resolution))
+                        if self.dr.aggregation == "mean":
+                            ds = resampled.mean()
+                        elif self.dr.aggregation == "max":
+                            ds = resampled.max()
+                        elif self.dr.aggregation == "min":
+                            ds = resampled.min()
+                        else:
+                            raise ValueError("Invalid temporal_aggregation")
+                    # spatial resample
+                    if self.dr.spatial_resolution > 0.25:
+                        c_f = int(self.dr.spatial_resolution / 0.25)
+                        coarsened = ds.coarsen(latitude=c_f, longitude=c_f, boundary="trim")
+                        if self.dr.aggregation == "mean":
+                            ds = coarsened.mean()
+                        elif self.dr.aggregation == "max":
+                            ds = coarsened.max()
+                        elif self.dr.aggregation == "min":
+                            ds = coarsened.min()
+                        else:
+                            raise ValueError("Invalid spatial_aggregation")
+                    ds_list.append(ds)
+
+            else:
+                raise RuntimeError(result.error)
+
+        # 5. assemble result
 
         # ds = xr.concat([i.chunk() for i in ds_list], dim="valid_time", join='outer')
         # compat="override" is a temporal walkaround as pre-aggregation value conflicts with downloaded data
--- backend/api/iharp_query_processor/src/remote/README.md
+++ backend/api/iharp_query_processor/src/remote/README.md
@@ -1,19 +1,14 @@
-# Remote Data Requests
+# Handling Remote Data Requests
 
 ### Outline
 
-- The `~/django-react-starter/backend/api/iharp_query_processor/src/query_executor_get_raster.py` checks if the storage has all the data requested in the current query
-
-- If (requested data - storage data) has leftovers, we need to request the remote repository for it. `query_executor_get_raster.py` finds the start and end {year, month, day} we need to ask for, and the `dr` class has information about the variable and dataset we want.
-
-- for each separate continuous region and time range, `query_executor_get_raster.py` will initialize an instance of the main remote data request driver `~/django-react-starter/backend/api/iharp_query_processor/src/remote/driver.py`
-
-- The driver will take in this information about the data, and call the necessary repository class to generate and handle the requests.
-
-- After the repository class returns confirmation that the data was downloaded successfully, the main driver will
-    - send the information to the query processor to begin the aggregation for the current query OR
-    - aggregate the data for the current query, then continue to aggregate to keep the data OR
-    - whatever we decide to do with the data
+1. A query calls `query_executor_get_raster.py` with the data it needs.
+2. We check if the needed data is in local storage. If some subset of the data is in storage, we get the relevant file names.
+3. If some subset of the data is missing, we:
+    * Determine the range of data we need to download,
+    * Call the `RequestRemoteData` driver to generate remote API requests, request and download the data, and return the downloaded file names.
+4. Join all the relevant files into one dataset
+5. Return the needed data.
 
 ### Description of remote directory files
 
@@ -45,3 +40,10 @@
 
             if dataset == "newdataset":
                 return NewDataset(self.config)
+
+### TODO
+* standardize downloaded datasets before returning them to the `query_executor_get_raster.py` OR make query executor more general.
+    * write function to turn lat/lon info into East or West domain for CARRA dataset
+* add `dataset` parameter to `DataRange` class.
+* allow multiple data regions/ranges to be needed for a query (so we do not have to just query one continuous region and time range).
+* make toml method an option to pass dict to `driver.py`.
